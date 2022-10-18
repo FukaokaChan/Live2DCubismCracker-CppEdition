7c9291e97cc744ef9646509e922c1e6ed847d6b9
Fukaoka Chanamaki <116107172+FukaokaChan@users.noreply.github.com>
2022-10-18 20:53:26 +0300

    Fixed founding file and fixed coping file

--- Live2DCubismCracker-CppEdition/find_live2dcubismRLM1221.hpp
+++ Live2DCubismCracker-CppEdition/find_live2dcubismRLM1221.hpp
@@ -5,6 +5,8 @@
 #include <filesystem>
 #include <fstream>
 using namespace std;
+wchar_t* t_path = (wchar_t*)L"C:\\Program Files\\Live2D Cubism 4.1\\app\\lib\\rlm1221.jar";
+wchar_t* x_path = (wchar_t*)L"C:\\Program Files\\Live2D Cubism 4.1\\app\\lib\\rlm1221.jar";
 LPTSTR path = (LPTSTR)LocalAlloc(LMEM_FIXED, (MAX_PATH + 1) * sizeof(TCHAR));
 TCHAR path_1[MAX_PATH + 1];
 
@@ -19,9 +21,13 @@ void FindFileLive2DCubismRLM1221() {
 		exit(122);
 	}
 	else {
+		HRESULT result = CopyFile2(t_path, x_path, 0);
 		file.read((char *)"rlm1221.jar", 0);
 		cout << "File Readed in Address: 0x" << file.hex << endl;
-		if (CopyFile2((PCWSTR)(wchar_t *)"rlm1221.jar", (PCWSTR)"C:\\Program Files\\Live2D Cubism 4.1\\app\\lib\\rlm1221.jar", 0)) {
+		if (result == 0) {
+			cout << "Error: File not Founded or Program not launched as Admin"
+		}
+		else {
 			cout << "Copied Successfully!!!" << endl;
 			Sleep(15000);
 			exit(444);
